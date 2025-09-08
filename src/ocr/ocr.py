96799623c5aeee17fcf9a502fd2742a4fabdaa00
sota-6741 <32445070@fukuchiyama.ac.jp>
2025-09-09 02:29:40 +0900

    add: PreProcessorクラスを追加し、画像の前処理メソッドを実装

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -38,3 +38,55 @@ class OCR():
 
         return pytesseract.image_to_string(pre_processed_image, lang="eng")
 
+class PreProcessor():
+    """
+    画像の前処理を行うためのユーティリティクラス
+
+    主な機能:
+        - グレースケール化
+        - 線形階調変換（コントラスト・明るさ調整）
+    """
+
+    @staticmethod
+    def apply_grayscale(image: np.ndarray):
+        """画像をグレイスケール化
+
+        Args:
+            image (np.ndarray): 入力画像
+        """
+
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    @staticmethod
+    def apply_lit(image: np.ndarray, alpha: float=1, beta: float=0):
+        """線形階調変換を画像に適用
+
+        Args:
+            image (np.ndarray): 入力画像
+            alpha (float): LUTのアルファ値
+            beta (float): LUTのベータ値
+
+        Returns:
+            np.ndarray: 階調変換後の画像(NupPy配列)
+        """
+
+        # テーブル作成
+        look_up_table = alpha * np.arange(256) + beta
+        # [0, 255]でクリップし，uint8型にする
+        look_up_table = np.clip(look_up_table, 0, 255).astype(np.uint8)
+
+        return cv2.LUT(image, look_up_table)
+
+    @staticmethod
+    def convert_cv2_to_pil(image: np.ndarray) -> Image.Image:
+        """
+        OpenCV形式（NumPy配列）の画像をPIL.Image形式に変換するメソッド。
+
+        Args:
+            image (np.ndarray): OpenCV形式の画像
+
+        Returns:
+            Image.Image: PIL形式の画像
+        """
+
+        return Image.fromarray(image)
